2443e65511eae79a31064e1d6f0e770143f37a6b
fridtjbs <71632344+fridtjbs@users.noreply.github.com>
2020-10-14 14:59:32 +0200

    opg1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import fs from 'fs';
 
+let inputN = document.getElementById('inputNavn');
+let inputE = document.getElementById('inputEmail');
 let studentList = document.getElementById('studentList');
 let students = [];
+let knapp = document.getElementById('Knapp');
 
 fs.readFile('src/data.json', (error, data) => {
   students = JSON.parse(data);
-
+  console.log(students);
   for (let student of students) {
     let li = document.createElement('li');
     li.innerText = student.name + ', ' + student.email;
     studentList.appendChild(li);
   }
 });
+knapp.onclick = (event) => {
+  students.push({
+    name: inputN.value,
+    email: inputE.value,
+  });
+  console.log(students);
+  var string = JSON.stringify(students);
+  console.log(string);
+  fs.writeFile('src/data.json', string, (err) => {
+    if (err) return console.log(err);
+  });
+
+  let li = document.createElement('li');
+  li.innerText = inputN.value + ', ' + inputE.value;
+  studentList.appendChild(li);
+};
